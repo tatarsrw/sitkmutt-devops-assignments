17dd63a590d93da1c655c5042b01b4bd08dda9fa
Siriwan Thongsuntiworakul <siriwan.tatar@mail.kmutt.ac.th>
2021-11-06 15:12:51 +0000

    Add Assignments1

---
+++ assignments/09-deploy-helm-mongodb-dev.sh
@@ -0,0 +1,27 @@
+# Custom [for ratings]
+
+kubectl create secret generic bookinfo-dev-ratings-mongodb-secret \
+  --from-literal=mongodb-password=CHANGEME \
+  --from-literal=mongodb-root-password=CHANGEME
+
+mkdir k8s/helm-values/
+
+touch k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml
+
+echo 'image:
+  tag: 5.0.3-debian-10-r30
+auth:
+  enabled: true
+  username: ratings
+  database: ratings
+  existingSecret: bookinfo-dev-ratings-mongodb-secret
+persistence:
+  enabled: false
+initdbScriptsConfigMap: bookinfo-dev-ratings-mongodb-initdb' > k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml
+
+kubectl create configmap bookinfo-dev-ratings-mongodb-initdb \
+  --from-file=databases/ratings_data.json \
+  --from-file=databases/script.sh
+
+helm install -f k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml \
+  bookinfo-dev-ratings-mongodb bitnami/mongodb --version 10.28.4
